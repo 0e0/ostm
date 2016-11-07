1990edeb632622e492ce7c085b8515b1a4a7aa7b
0e0 <ejersonb@gmail.com>
2016-11-07 13:02:22 -0600

    yes

--- webpack.config.js
+++ webpack.config.js
@@ -10,28 +10,34 @@ module.exports = {
   },
   module: {
     loaders: [{
-        test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: 'babel',
         query: {
           presets: ['react', 'es2015', 'stage-1']
         }
       },
       {
         test: /\.less$/,
-        loaders: ['style', 'css', 'less'],
+        loader: 'style!css!less'
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loaders: [
             'file?hash=sha512&digest=hex&name=[hash].[ext]',
             'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-        ]
+        ],
     }]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx', '.less'],
+    modulesDirectories: [
+      'node_modules'
+    ]
   },
+  resolveLoader: {
+    root: path.resolve(__dirname, 'node_modules')
+  },
+  
   devServer: {
     historyApiFallback: true,
     contentBase: './'
