11331ba7358bec1c1cf2e489d8954080ed16ae6d
0e0 <ejersonb@gmail.com>
2016-11-07 13:25:28 -0600

    yeye

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,6 @@
-const path = require('path');
-const webpack = require('webpack');
-
-
 module.exports = {
   entry: [
-    'index.js'
+    './src/index.js'
   ],
   output: {
     path: __dirname,
@@ -13,12 +9,10 @@ module.exports = {
   },
   module: {
     loaders: [{
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-1']
-        }
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
       },
       {
         test: /\.less$/,
@@ -33,17 +27,59 @@ module.exports = {
     }]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx', '.less'],
-    modulesDirectories: [
-      'node_modules'
-    ]
+    extensions: ['', '.js', '.jsx']
   },
-  resolveLoader: {
-    root: path.resolve(__dirname, 'node_modules')
-  },
-
   devServer: {
     historyApiFallback: true,
     contentBase: './'
   }
 };
+
+
+
+
+// module.exports = {
+//   entry: [
+//     'index.js'
+//   ],
+//   output: {
+//     path: __dirname,
+//     publicPath: '/',
+//     filename: 'bundle.js'
+//   },
+//   module: {
+//     loaders: [{
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         loader: 'babel-loader',
+//         query: {
+//           presets: ['react', 'es2015', 'stage-1']
+//         }
+//       },
+//       {
+//         test: /\.less$/,
+//         loader: 'style!css!less'
+//       },
+//       {
+//         test: /\.(jpe?g|png|gif|svg)$/i,
+//         loaders: [
+//             'file?hash=sha512&digest=hex&name=[hash].[ext]',
+//             'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+//         ],
+//     }]
+//   },
+//   resolve: {
+//     extensions: ['', '.js', '.jsx', '.less'],
+//     modulesDirectories: [
+//       'node_modules'
+//     ]
+//   },
+//   resolveLoader: {
+//     root: path.resolve(__dirname, 'node_modules')
+//   },
+//
+//   devServer: {
+//     historyApiFallback: true,
+//     contentBase: './'
+//   }
+// };
